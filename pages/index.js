6ec9e36d69d50deceb7604bea1fd369584b88ed6
Abner Cezar <102832541+abnercezar@users.noreply.github.com>
2024-04-28 18:08:31 +0000

    adicionado a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home() {
+    return <h1 style={{ backgroundColor: '#FFD700', padding: '10px', borderRadius: '10px' }}> ❤️ Sou muito feliz por ter você como minha esposa! Te Amo!! 💃💖 </h1>;
+}
+
+export default Home;
