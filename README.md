f62476355088c0110ac97d4e80f40e17792858ea
Abner Cezar <102832541+abnercezar@users.noreply.github.com>
2024-05-02 03:13:25 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto do curso https://www.tabnews.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
-    return <h1 style={{ backgroundColor: '#FFD700', padding: '10px', borderRadius: '10px' }}> ❤️ Sou muito feliz por ter você como minha esposa! Te Amo!! 💃💖 </h1>;
+  return (
+    <h1
+      style={{
+        backgroundColor: "#FFD700",
+        padding: "10px",
+        borderRadius: "10px",
+      }}
+    >
+      {" "}
+      ❤️ Sou muito feliz por ter você como minha esposa! Te Amo!! 💃💖{" "}
+    </h1>
+  );
 }
 
 export default Home;
