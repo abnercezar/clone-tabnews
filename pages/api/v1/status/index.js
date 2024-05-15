b898ad7277a04f06d431c294d00ee68cd3f269b0
Abner Cezar <abnercezar30@gmail.com>
2024-05-14 23:02:21 -0300

    finalish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,49 @@
+// Controller
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result);
-  response.status(200).json({ chave: "são acima da média" });
+  const updateAt = new Date().toISOString();
+
+  // Consulta o banco de dados para obter a versão do servidor
+  const databaseVersionResult = await database.query("SHOW server_version;");
+
+  // Extrai a versão do servidor do resultado da consulta
+  const databaseVersionValue =
+    await databaseVersionResult.rows[0].server_version;
+
+  // Consulta o banco de dados para obter o número máximo de conexões
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+
+  // Extrai o número máximo de conexões do resultado da consulta
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  // Obtém o nome do banco de dados a partir das variáveis de ambiente
+  const databaseName = process.env.POSTGRES_DB;
+
+  // Consulta o banco de dados para obter o número de conexões abertas para esse banco de dados
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  // Extrai o número de conexões abertas do resultado da consulta
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  //Inclui a versão na resposta
+  response.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,24 @@
+// Teste: "GET para api/v1/status deve retornar 200"
 test("GET to api/v1/status should return 200", async () => {
+  // Faz uma requisição GET para a rota api/v1/status
   const response = await fetch("http://localhost:3000/api/v1/status");
+  // Espera que o status da resposta seja 200
   expect(response.status).toBe(200);
+  // Converte a resposta para JSON
+  const responseBody = await response.json();
+
+  // Converte a data de atualização recebida da resposta em um formato de string ISO.
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  // Compara se a data de atualização da resposta é igual à data convertida para o formato ISO.
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  // Verifica se a versão do banco de dados na resposta é igual a "16.0".
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  // Verifica se o número máximo de conexões do banco de dados na resposta é igual a 100.
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+  // Verifica se o número de conexões abertas do banco de dados na resposta é igual a 1.
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
