3f5c4f30dc5a9d9f5d897098c6276f9023f1c7c3
Abner Cezar <abnercezar30@gmail.com>
2024-05-14 23:00:07 -0300

    make database.js more robust to error handing

--- infra/database.js
+++ infra/database.js
@@ -1,5 +1,6 @@
 import { Client } from "pg";
 
+//Função assíncrona para executar uma consulta no banco de dados.
 async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
@@ -9,9 +10,14 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
