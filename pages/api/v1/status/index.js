a29b5fff51a027f837261403ade30b93bd520554
Abner Cezar <abnercezar30@gmail.com>
2025-01-30 22:44:46 -0300

    refactor: standardizes the Controllers for the `/migrations` and `status` endpoints

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,64 +1,58 @@
 // Controller
+import { createRouter } from "next-connect";
 import database from "infra/database";
-import { InternalServerError } from "infra/errors";
+import controller from "infra/controller.js";
 
-async function status(request, response) {
-  try {
-    const updateAt = new Date().toISOString();
+// Cria uma instância do roteador
+const router = createRouter();
 
-    // Consulta o banco de dados para obter a versão do servidor
-    const databaseVersionResult = await database.query("SHOW server_version;");
+// Define o manipulador para o método GET
+router.get(getHandler);
 
-    // Extrai a versão do servidor do resultado da consulta
-    const databaseVersionValue =
-      await databaseVersionResult.rows[0].server_version;
+// Exporta o roteador com os manipuladores de erro personalizados
+export default router.handler(controller.errorHandlers);
 
-    // Consulta o banco de dados para obter o número máximo de conexões
-    const databaseMaxConnectionsResult = await database.query(
-      "SHOW max_connections;",
-    );
+async function getHandler(request, response) {
+  const updateAt = new Date().toISOString();
 
-    // Extrai o número máximo de conexões do resultado da consulta
-    const databaseMaxConnectionsValue =
-      databaseMaxConnectionsResult.rows[0].max_connections;
+  // Consulta o banco de dados para obter a versão do servidor
+  const databaseVersionResult = await database.query("SHOW server_version;");
 
-    // Obtém o nome do banco de dados a partir das variáveis de ambiente
-    const databaseName = process.env.POSTGRES_DB;
+  // Extrai a versão do servidor do resultado da consulta
+  const databaseVersionValue =
+    await databaseVersionResult.rows[0].server_version;
 
-    // Consulta o banco de dados para obter o número de conexões abertas para esse banco de dados
-    const databaseOpenedConnectionsResult = await database.query({
-      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-      values: [databaseName],
-    });
+  // Consulta o banco de dados para obter o número máximo de conexões
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
 
-    // Extrai o número de conexões abertas do resultado da consulta
-    const databaseOpenedConnectionsValue =
-      databaseOpenedConnectionsResult.rows[0].count;
+  // Extrai o número máximo de conexões do resultado da consulta
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
 
-    //Inclui a versão na resposta
-    response.status(200).json({
-      updated_at: updateAt, // Data de atualização
-      dependencies: {
-        database: {
-          version: databaseVersionValue, // Versão do banco de dados
-          max_connections: parseInt(databaseMaxConnectionsValue), // Máximo de conexões permitidas
-          opened_connections: databaseOpenedConnectionsValue, // Conexões abertas atualmente
-        },
+  // Obtém o nome do banco de dados a partir das variáveis de ambiente
+  const databaseName = process.env.POSTGRES_DB;
+
+  // Consulta o banco de dados para obter o número de conexões abertas para esse banco de dados
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  // Extrai o número de conexões abertas do resultado da consulta
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  //Inclui a versão na resposta
+  response.status(200).json({
+    updated_at: updateAt, // Data de atualização
+    dependencies: {
+      database: {
+        version: databaseVersionValue, // Versão do banco de dados
+        max_connections: parseInt(databaseMaxConnectionsValue), // Máximo de conexões permitidas
+        opened_connections: databaseOpenedConnectionsValue, // Conexões abertas atualmente
       },
-    });
-  } catch (error) {
-    // Cria um objeto de erro público com a causa do erro
-    const publicErrorObject = new InternalServerError({
-      cause: error,
-    });
-
-    // Loga o erro no console
-    console.log("\n Erro dentro do catch do controller:");
-    console.error(publicErrorObject);
-
-    // Retorna uma resposta de erro 500 com o objeto de erro público
-    response.status(500).json(publicErrorObject);
-  }
+    },
+  });
 }
-
-export default status;
