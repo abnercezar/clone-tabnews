c0d04e0738f514e3aedcabe8448ddf3c41fe269f
Abner Cezar <abnercezar30@gmail.com>
2025-04-30 21:55:44 -0300

    refactor: remove pepper `password.js`, create mvp `index.js` and adjust comments in `database.js`

--- models/password.js
+++ models/password.js
@@ -1,14 +1,10 @@
 import bcryptjs from "bcryptjs";
 import "dotenv/config";
 
-// Recupera o valor do PEPPER a partir das variáveis de ambiente
-const PEPPER = process.env.PEPPER || "";
-
-// Função para gerar o hash de uma senha com base no número de rounds e PEPPER
+// Função para gerar o hash de uma senha com base no número de rounds
 async function hash(password) {
   const rounds = getNumberOfRounds();
-  const pepperedPassword = password + PEPPER;
-  return await bcryptjs.hash(pepperedPassword, rounds);
+  return await bcryptjs.hash(password, rounds);
 }
 
 // Define o número de rounds para o hash, dependendo do ambiente (produção ou desenvolvimento)
@@ -16,10 +12,9 @@ function getNumberOfRounds() {
   return process.env.NODE_ENV === "production" ? 14 : 1;
 }
 
-// Compara uma senha fornecida com o hash armazenado (aplicando o PEPPER antes)
+// Compara uma senha fornecida com o hash armazenado
 async function compare(providedPassword, storedPassword) {
-  const pepperedPassword = providedPassword + PEPPER;
-  return await bcryptjs.compare(pepperedPassword, storedPassword);
+  return await bcryptjs.compare(providedPassword, storedPassword);
 }
 
 // Exporta as funções hash e compare como parte do objeto password
