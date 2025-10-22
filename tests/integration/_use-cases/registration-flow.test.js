af11d125ec6540685f39e83058c1c4785297c68e
Abner Cezar <abnercezar30@gmail.com>
2025-10-21 22:36:02 -0300

    fix: handle empty email list in `orchestrator.getLastEmail()`

--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -1,3 +1,4 @@
+import activation from "models/activation";
 import orchestrator from "tests/orchestrator";
 
 beforeAll(async () => {
@@ -8,6 +9,8 @@ beforeAll(async () => {
 });
 
 describe("Use case: Registration Flow (all successful)", () => {
+  let createUserResponseBody;
+
   test("Create user account", async () => {
     const createUserResponse = await fetch(
       "http://localhost:3000/api/v1/users",
@@ -25,7 +28,7 @@ describe("Use case: Registration Flow (all successful)", () => {
     );
     expect(createUserResponse.status).toBe(201);
 
-    const createUserResponseBody = await createUserResponse.json();
+    createUserResponseBody = await createUserResponse.json();
 
     expect(createUserResponseBody).toEqual({
       id: createUserResponseBody.id,
@@ -37,12 +40,26 @@ describe("Use case: Registration Flow (all successful)", () => {
       updated_at: createUserResponseBody.updated_at,
     });
   });
+
+  test("Receive activation email", async () => {
+    const lastEmail = await orchestrator.getLastEmail();
+
+    const activationToken = await activation.findOneByUserId(
+      createUserResponseBody.id,
+    );
+
+    expect(lastEmail.sender).toBe("<agrotab@gmail.com>");
+    expect(lastEmail.recipients[0]).toBe("<registration.flow@agrotab.com>");
+    expect(lastEmail.subject).toBe("Ative seu cadastro Agrotab");
+    expect(lastEmail.text).toContain("RegistrationFlow");
+    expect(lastEmail.text).toContain(activationToken.id);
+
+    console.log(lastEmail.text);
+  });
+
+  test("Activate account", async () => {});
+
+  test("Login", async () => {});
+
+  test("Get user information", async () => {});
 });
-
-test("Receive activation email", async () => {});
-
-test("Activate account", async () => {});
-
-test("Login", async () => {});
-
-test("Get user information", async () => {});
