758693b2883643505ef6e6a588855aea3b7ed15c
Abner Cezar <abnercezar30@gmail.com>
2026-05-06 22:08:07 -0300

    refactor: improve erro loggin in `email.send()`

--- infra/errors.js
+++ infra/errors.js
@@ -22,14 +22,15 @@ export class InternalServerError extends Error {
 }
 
 export class ServiceError extends Error {
-  constructor({ cause, message }) {
+  constructor({ cause, message, action, context }) {
     // Chama o construtor da classe pai (Error) com uma mensagem padrão e a causa do erro
     super(message || "Serviço indisponível no momento.", {
       cause,
     });
     this.name = "ServiceError";
-    this.action = "Verifique se o serviço está disponível.";
+    this.action = action || "Verifique se o serviço está disponível.";
     this.statusCode = 503;
+    this.context = context;
   }
 
   // Converte o erro para um formato JSON
@@ -39,6 +40,7 @@ export class ServiceError extends Error {
       message: this.message,
       action: this.action,
       status_code: this.statusCode,
+      context: this.context,
     };
   }
 }
